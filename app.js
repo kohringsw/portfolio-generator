5fec720443920607523b117b7111c8cf740cbc1f
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2020-11-29 22:31:33 -0600

    fixed bug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { writeFile, copyFile } = require(".utils/generate-site");
+const { writeFile, copyFile } = require(".utils/generate-site.js");
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
